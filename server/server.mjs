8e9c0121e8545246a59ce519d367d9a7a0b2165c
ruth-dvops <ruthyasmine1506@gmail.com>
2025-05-21 14:35:58 +0800

    did backend for questions.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -3,7 +3,8 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import players from './routes/players.mjs';
-import  admins from './routes/admins.mjs';
+import admins from './routes/admins.mjs';
+import questions from './routes/questions.mjs';
 
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/players", players);
 app.use("/admins", admins);
+app.use("/questions", questions);
 
 // Connect to MongoDB
 mongoose.set('strictQuery', true);
