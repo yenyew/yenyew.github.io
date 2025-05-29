cea1e7ce9a62c52e8cc3d0de09c7b57810ea78f8
yenyew <2300927g@student.tp.edu.sg>
2025-05-29 13:56:15 +0800

    Created login screen without functions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import { Route, Routes} from "react-router-dom" ;
 import './App.css'
 import HomePage from './components/HomePage.jsx';
+import LoginScreen from './components/Login.jsx';
 
 
 export default function App () {
   return (
     <div className="app">
-    <Routes>
-        <Route path="/" element={<HomePage />} />
-    </Routes>
+    <LoginScreen />
     </div>
   )
 }
