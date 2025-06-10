ee20b08b83bbf06cf6c40607ff38837ff78e9898
ruth-dvops <ruthyasmine1506@gmail.com>
2025-06-10 19:48:38 +0800

    connected the backend questions schema to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes} from "react-router-dom" ;
 import './App.css'
 import HomePage from './components/HomePage.jsx';
+import QuestionPage from './components/QuestionPage.jsx';
 
 
 export default function App () {
   return (
     <div className="app">
     <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<QuestionPage />} />
     </Routes>
     </div>
   )
