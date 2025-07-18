37b0ac4c330ee4ae1301219b8da76e27d67c4c35
kerxuntp <kerxunteo1@gmail.com>
2025-07-19 01:35:51 +0800

    improved UI on badusernames, global settings and leaderboard admin, leaderboard admin still wip but most of it can use alertmodal

--- client/src/components/EditCollection.jsx
+++ client/src/components/EditCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "./MainStyles.css";
+
 const EditCollection = () => {
   const { id } = useParams();
   const [name, setName] = useState("");
