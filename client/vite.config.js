5149c7eefb1977f0846e9860afda8cf3c5a2edab
kerxuntp <kerxunteo1@gmail.com>
2025-08-03 17:26:46 +0800

    adjust for hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This is what Vercel expects as the output folder
+  },
+  server: {
+    port: 5173, // Optional: for local dev only
+  },
+});
